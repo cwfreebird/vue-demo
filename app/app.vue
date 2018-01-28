c847281f937a2dbd286c921006b100982dc53887
david.cai <david.cai@vipshop.com>
2018-01-28 16:46:33 +0800

    first commit

---
+++ app/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-title title="Vue组件化"></v-title>
+        <v-button @click="handleClick">点击按钮</v-button>
+    </div>
+</template>
+<script>
+    import vButton from './button.vue'
+    import vTitle from './title.vue'
+
+    export default {
+        components: {
+            vButton,
+            vTitle,
+        },
+        methods: {
+            handleClick (e) {
+                console.log(e)
+            }
+        }
+    }
+</script>
